5aebdde9534297b8cc7fc3ef948ff9461a7ac0e0
N8im <nerdgamerbro666@gmail.com>
2025-04-16 15:28:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View, TouchableOpacity, TextInput, Image, FlatList } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import styles from './style.js';
-import DATA from './movies';
+import DATA from './movies.js';
 import Cabecalho from './src/componentes/cabecalho/index.js';
 import Barradepesquisa  from './src/componentes/pesquisa/index.js';
 import CardsFilmes from './src/componentes/cardsFilmes.js/index.js';
