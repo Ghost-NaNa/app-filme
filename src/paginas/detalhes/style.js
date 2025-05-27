e2087b812252d00b76c9b58d720e0d56d3eb5740
Nathan <nathan.oltavares@gmail.com>
2025-05-27 16:35:51 -0300

    Finalização da página de detalhes

--- src/paginas/detalhes/style.js
+++ src/paginas/detalhes/style.js
@@ -13,35 +13,44 @@ const estilo = StyleSheet.create(
             textAlign: 'center',
             padding: '8px',
             color: 'white'
-        }, 
+        },
         imageContainer: {
             flexDirection: 'row',
             justifyContent: 'space-around',
             gap: 10,
+            padding: '16px'
         },
         image: {
-        width: 150,
-        height: 220, 
-        borderRadius: 6,
-        marginBottom: 10,
-        marginLeft: 30
-    },
+            width: 150,
+            height: 220,
+            borderRadius: 6,
+            marginBottom: 10,
+            marginLeft: 30
+        },
         detalhes: {
             marginRight: 40,
+            padding: '16px',
+            borderRadius: 8,
             width: '43%',
-            height: '90%',
+            height: 220,
             flexDirection: 'column',
             justifyContent: 'space-between',
             gap: 13,
-            color: 'white'
+            color: 'white',
+            backgroundColor: '#2B2242'
         },
         detalhesTexto: {
-            color:'white'
+            color: 'white'
         },
         containerSinopse: {
             backgroundColor: '#141a29',
             padding: 20,
-            flexDirection: 'column'
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            backgroundColor: '#2B2242',
+            marginLeft: 16,
+            marginRight: 16,
+            borderRadius: 8
         }
     }
 )
