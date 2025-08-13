af58043431b4fd81f764a1b1b56bad357c8c769a
Nathan <nathan.oltavares@gmail.com>
2025-08-13 13:51:28 -0300

    Add files via upload

---
+++ cardsFilmes/index.js
@@ -0,0 +1,17 @@
+import { Image, Text, TouchableOpacity, FlatList } from "react-native";
+import DATA from "../../../movies.js";
+import { useNavigation } from "@react-navigation/native";
+import estiloCard from "./estiloCards.js";
+
+export default function CardsFilmes({ titulo, nota, imagem, genero, autor, lancamento, sinopse }) {
+
+    const navigation = useNavigation();
+
+    return (
+        <TouchableOpacity style={estiloCard.containerFilmes} onPress={() => navigation.navigate("Detalhes", { titulo, nota, imagem, genero, autor, lancamento, sinopse })}>
+            <Image source={{uri:`https://image.tmdb.org/t/p/original/${imagem}`}} style={estiloCard.images} />
+            <Text style={estiloCard.titulo}> {titulo} </Text>
+            <Text style={estiloCard.textNota}> Avaliação no IMDB: {nota} </Text>
+        </TouchableOpacity>
+    )
+}
